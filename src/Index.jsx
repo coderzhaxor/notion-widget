0077e6dbe9ce15c11a870d2b1d351d384ec92ac6
coderzhaxor <ajanuantara@gmail.com>
2023-02-21 22:18:25 +0700

    Create UI

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import Clock from "../pages/Clock/Clock";
+import "./index.css";
 
 function Index() {
   const [time, setTime] = useState(0);
+  const [rounded, setRounded] = useState(0);
 
   return (
     <div className="Index">
@@ -14,7 +16,41 @@ function Index() {
       <div className="container">
         <h3 style={{ fontWeight: 600 }}> Digital Clock </h3>
       </div>
-      <Clock />
+      <Clock rounded={rounded} />
+      <div className="custom__menu">
+        <div className="custom__color custom__items">
+          <img src="./text.svg" alt="text color" className="input__color" />
+          <input
+            type="text"
+            name="color_value"
+            id="color_value"
+            placeholder="#212121"
+          />
+        </div>
+        <div className="custom__rounded custom__items">
+          <img src="./rounded.svg" alt="rounded logo" />
+          <input
+            type="range"
+            name="input__rounded"
+            id="input__rounded"
+            min="0"
+            max="30"
+            onChange={() =>
+              setRounded(document.getElementById("input__rounded").value)
+            }
+            className="range__border"
+          />
+        </div>
+        <div className="custom__border custom__items">
+          <img src="./border.svg" alt="border logo" className="border__color" />
+          <input
+            type="text"
+            name="border__range"
+            id="border__range"
+            placeholder="#212121"
+          />
+        </div>
+      </div>
     </div>
   );
 }
