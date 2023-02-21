7df510d1c00c79cf0c212956ad016165e3de8f22
coderzhaxor <ajanuantara@gmail.com>
2023-02-21 09:39:35 +0700

    Adding customize home wip 50%

--- src/Index.jsx
+++ src/Index.jsx
@@ -6,6 +6,14 @@ function Index() {
 
   return (
     <div className="Index">
+      <header>
+        <div className="line"></div>
+        <img src="../public/logo.svg" alt="logo" className="logo" />
+        <div className="line"></div>
+      </header>
+      <div className="container">
+        <h3 style={{ fontWeight: 600 }}> Digital Clock </h3>
+      </div>
       <Clock />
     </div>
   );
